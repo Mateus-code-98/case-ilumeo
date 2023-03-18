bc6449b9b3aca1a3261973b8ddf5f76683640a02
mateus-code-98 <teu-9@hotmail.com>
2023-03-18 04:09:28 -0300

    Add check route verification if there is a check in progress

--- src/modules/users/routes/utils/index.ts
+++ src/modules/users/routes/utils/index.ts
@@ -1,6 +1,8 @@
 import { IRoutesProps } from "../../../../shared/interfaces";
+import { getCheckInProgressController } from "../../controllers/getCheckInProgress.controller";
 import { getUserWorkingHoursController } from "../../controllers/getUserWorkingHours.controller";
 
 export const _routes: IRoutesProps[] = [
-	{ method: "get", url: "/working-hours", middlewares: [getUserWorkingHoursController] }
+	{ method: "get", url: "/working-hours", middlewares: [getUserWorkingHoursController] },
+	{ method: "get", url: "/check-in-progress", middlewares: [getCheckInProgressController] }
 ];
